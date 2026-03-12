5d6d1c389c821ea187031517ca728f90bfb24281
heshsaih <heshsaih@gmail.com>
2026-03-12 21:51:30 +0100

    feat: proper letter and word highlighing in response to typing

--- frontend/src/pages/typing/text-container.tsx
+++ frontend/src/pages/typing/text-container.tsx
@@ -11,7 +11,7 @@ type CursorPosition = {
     l: number;
 };
 
-type WordStatus = "NONE" | "CORRET" | "INCORRECT";
+type WordStatus = "NONE" | "CORRECT" | "INCORRECT";
 type LetterStatus = "NONE" | "CORRECT" | "INCORRECT";
 
 type Word = {
@@ -22,6 +22,184 @@ type Word = {
 type Letter = {
     letter: string;
     status: LetterStatus;
+};
+
+const getWordColor = (status: WordStatus) => {
+    switch (status) {
+        case "NONE":
+            return "";
+        case "INCORRECT":
+            return "underline decoration-error";
+        case "CORRECT":
+            return "";
+    }
+};
+
+const getLetterColor = (status: LetterStatus) => {
+    switch (status) {
+        case "NONE":
+            return "text-text-disabled";
+        case "INCORRECT":
+            return "text-error";
+        case "CORRECT":
+            return "text-text";
+    }
+};
+
+const setLettersStatus = (
+    words: Word[],
+    wi: number,
+    listart: number,
+    liend: number,
+    status: LetterStatus,
+): Word[] => {
+    if (listart >= words[wi].letters.length || liend > words[wi].letters.length) {
+        return words;
+    }
+
+    for (let i = listart; i < liend; i++) {
+        words[wi].letters[i].status = status;
+    }
+
+    return words;
+};
+
+const handleKeyboardClick = (
+    e: KeyboardEvent,
+    words: Word[],
+    currentCursorPos: CursorPosition,
+): {
+    newCursorPos: CursorPosition;
+    words: Word[];
+} => {
+    let newCursorPos = currentCursorPos;
+    if (e.code === "Backspace") {
+        if (currentCursorPos.w === 0 && currentCursorPos.l === 0) {
+            return {
+                newCursorPos: currentCursorPos,
+                words,
+            };
+        }
+
+        if (currentCursorPos.l === 0) {
+            const newCursorPosW = currentCursorPos.w - 1;
+            let newCursorPosL = 0;
+            if (e.ctrlKey) {
+                newCursorPosL = 0;
+            } else {
+                for (let i = words[newCursorPosW].letters.length - 1; i >= 0; i--) {
+                    if (words[newCursorPosW].letters[i].status !== "NONE") {
+                        newCursorPosL = i + 1;
+                        break;
+                    }
+                }
+            }
+
+            newCursorPos = {
+                w: newCursorPosW,
+                l: newCursorPosL,
+            };
+        } else {
+            newCursorPos = {
+                w: currentCursorPos.w,
+                l: e.ctrlKey ? 0 : currentCursorPos.l - 1,
+            };
+        }
+
+        if (e.ctrlKey) {
+            words[newCursorPos.w].status = "NONE";
+            setLettersStatus(
+                words,
+                newCursorPos.w,
+                0,
+                words[newCursorPos.w].letters.length,
+                "NONE",
+            );
+        } else {
+            setLettersStatus(
+                words,
+                newCursorPos.w,
+                newCursorPos.l,
+                newCursorPos.l + 1,
+                "NONE",
+            );
+
+            if (
+                !words[newCursorPos.w].letters.find(
+                    (letter) => letter.status === "INCORRECT",
+                )
+            ) {
+                words[newCursorPos.w].status = "CORRECT";
+            }
+        }
+
+        return {
+            newCursorPos,
+            words,
+        };
+    }
+
+    if (e.code === "Space") {
+        newCursorPos = {
+            w: currentCursorPos.w + 1,
+            l: 0,
+        };
+
+        if (currentCursorPos.l < words[currentCursorPos.w].letters.length) {
+            words[currentCursorPos.w].status = "INCORRECT";
+        }
+
+        return {
+            newCursorPos,
+            words,
+        };
+    }
+
+    if (e.key.length !== 1) {
+        return {
+            newCursorPos,
+            words,
+        };
+    }
+
+    const keyCode = e.key.charCodeAt(0);
+    if (!(keyCode >= 65 && keyCode <= 90) && !(keyCode >= 97 && keyCode <= 122)) {
+        return {
+            newCursorPos,
+            words,
+        };
+    }
+
+    if (words[newCursorPos.w].letters[newCursorPos.l].letter === e.key) {
+        words[newCursorPos.w].letters[newCursorPos.l].status = "CORRECT";
+        if (
+            !words[newCursorPos.w].letters.find(
+                (letter) => letter.status === "INCORRECT",
+            )
+        ) {
+            words[newCursorPos.w].status = "CORRECT";
+        }
+    } else {
+        words[newCursorPos.w].letters[newCursorPos.l].status = "INCORRECT";
+        words[newCursorPos.w].status = "INCORRECT";
+    }
+
+    if (currentCursorPos.l >= words[currentCursorPos.w].letters.length) {
+        newCursorPos = {
+            w: currentCursorPos.w + 1,
+            l: 0,
+        };
+    } else {
+        newCursorPos = {
+            w: currentCursorPos.w,
+            l: currentCursorPos.l + 1,
+        };
+    }
+
+    return {
+        newCursorPos,
+        words,
+    };
 };
 
 export const TextContainer: FC<TextContainerProps> = ({ words }) => {
@@ -42,53 +220,9 @@ export const TextContainer: FC<TextContainerProps> = ({ words }) => {
 
     useEffect(() => {
         const handleClick = (e: KeyboardEvent) => {
-            if (e.code === "Backspace") {
-                if (cursorPos.l === 0) {
-                    setCursorPos({
-                        w: cursorPos.w - 1,
-                        l: e.ctrlKey ? 0 : mappedWords[cursorPos.w - 1].letters.length,
-                    });
-                    return;
-                }
-                setCursorPos({
-                    w: cursorPos.w,
-                    l: e.ctrlKey ? 0 : cursorPos.l - 1,
-                });
-                return;
-            }
-
-            if (e.code === "Space") {
-                setCursorPos({
-                    w: cursorPos.w + 1,
-                    l: 0,
-                });
-                return;
-            }
-
-            if (e.key.length !== 1) {
-                return;
-            }
-
-            const keyCode = e.key.charCodeAt(0);
-            if (
-                !(keyCode >= 65 && keyCode <= 90) &&
-                !(keyCode >= 97 && keyCode <= 122)
-            ) {
-                return;
-            }
-
-            if (cursorPos.l >= mappedWords[cursorPos.w].letters.length) {
-                setCursorPos({
-                    w: cursorPos.w + 1,
-                    l: 0,
-                });
-
-                return;
-            }
-            setCursorPos({
-                w: cursorPos.w,
-                l: cursorPos.l + 1,
-            });
+            const result = handleKeyboardClick(e, mappedWords, cursorPos);
+            setMappedWords(result.words);
+            setCursorPos(result.newCursorPos);
         };
 
         document.addEventListener("keydown", handleClick);
@@ -97,18 +231,16 @@ export const TextContainer: FC<TextContainerProps> = ({ words }) => {
         };
     }, [cursorPos]);
 
-    console.log(cursorPos);
-
     return (
         <div className="relative text-3xl w-11/12 text-center overflow-visible">
             {mappedWords.map((w, wi) => (
                 <span className="inline-block">
                     <span className="whitespace-pre"> </span>
                     {cursorPos.w === wi && cursorPos.l === 0 && <Cursor></Cursor>}
-                    <span className="">
+                    <span className={getWordColor(w.status)}>
                         {w.letters.map((l, li) => (
                             <>
-                                <span className="text-text-disabled">{l.letter}</span>
+                                <span className={getLetterColor(l.status)}>{l.letter}</span>
                                 {cursorPos.w === wi && cursorPos.l - 1 === li && (
                                     <Cursor></Cursor>
                                 )}
