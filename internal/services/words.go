1592368f74097df4de66743182565bfdb1f0ff66
heshsaih <heshsaih@gmail.com>
2026-03-17 21:14:44 +0100

    feat: word API replaced

--- internal/services/words.go
+++ internal/services/words.go
@@ -1,33 +1,47 @@
 package services
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
-	"strings"
 	"typers/internal/enums"
 )
 
+type ApiResponse []struct {
+	Word     string
+	Length   int
+	Category string
+	Language string
+}
+
 func GetWords(amount int) ([]string, error) {
-	wordApi := os.Getenv("WORD_API");
-	if (len(wordApi) == 0) {
+	wordApi := os.Getenv("WORD_API")
+	if len(wordApi) == 0 {
 		return nil, errors.New(string(enums.ERR_WORD_API_UNAVAILABLE))
 	}
 
-	resp, err := http.Get(fmt.Sprintf("%s/word?number=%d", wordApi, amount))
-	if (err != nil) {
+	resp, err := http.Get(fmt.Sprintf("%s?language=en&words=%d", wordApi, amount))
+	if err != nil {
 		return nil, errors.New(string(enums.ERR_WORD_API_UNAVAILABLE))
 	}
 
 	body, err := io.ReadAll(resp.Body)
-	if (err != nil) {
+	if err != nil {
 		return nil, errors.New(string(enums.ERR_WORD_API_UNAVAILABLE))
 	}
 
-	noQuotes:= strings.ReplaceAll(string(body), "\"", "")
-	trimmed := strings.TrimRight(strings.TrimLeft(noQuotes, "["), "]")
+	var parsedApiResponse ApiResponse
+	if err := json.Unmarshal(body, &parsedApiResponse); err != nil {
+		return nil, errors.New(string(enums.ERR_WORD_API_UNAVAILABLE))
+	}
 
-	return strings.Split(trimmed, ","), nil
+	var response []string
+	for i := range parsedApiResponse {
+		response = append(response, parsedApiResponse[i].Word)
+	}
+
+	return response, nil
 }
