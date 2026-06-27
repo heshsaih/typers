f01522071fa9c6ce61e5ab94a401551079ac52d0
heshsaih <heshsaih@gmail.com>
2026-06-27 22:44:37 +0200

    feat: database connection

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"log"
+	"os"
+	"strconv"
+	"typers/internal/database"
 	"typers/internal/handlers"
 	"typers/internal/service"
 
@@ -17,9 +20,27 @@ func main() {
 
 	service.LoadWords()
 
+	databaseCfg := database.DatabseConnectionConfig{
+		Host:         os.Getenv("DB_HOST"),
+		User:         os.Getenv("DB_USERNAME"),
+		Password:     os.Getenv("DB_PASSWORD"),
+		DatabaseName: os.Getenv("DB_NAME"),
+	}
+	if dbPort, err := strconv.Atoi(os.Getenv("DB_PORT")); err != nil {
+		log.Fatal("failed to parse database port from env")
+	} else {
+		databaseCfg.Port = dbPort
+	}
+
+	database.Connect(databaseCfg)
+
 	router := gin.Default()
 	router.Use(cors.Default())
 	v1 := router.Group("/api/v1")
+
+	//auth
+	v1.POST("/auth/signin", handlers.HandleSignIn)
+	v1.POST("/auth/login", handlers.HandleLogin)
 
 	//words
 	v1.GET("/words", handlers.HandleGetWords)
---
+++ internal/database/database.go
@@ -0,0 +1,46 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type DatabseConnectionConfig struct {
+	Host         string
+	Port         int
+	User         string
+	Password     string
+	DatabaseName string
+}
+
+var database *gorm.DB = nil
+
+func Connect(cfg DatabseConnectionConfig) {
+	if database != nil {
+		log.Fatal("the app is already connected to a db, this should never happen")
+	}
+
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=disable TimeZone=Europe/Warsaw", cfg.Host, cfg.User, cfg.Password, cfg.DatabaseName, cfg.Port)
+	fmt.Println(dsn)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		TranslateError: true,
+	})
+	if err != nil {
+		log.Fatal("failed to connect to the db", err)
+	}
+
+	log.Println("connection to the db established")
+	database = db
+}
+
+func GetConnection() *gorm.DB {
+	if database == nil {
+		log.Fatal("no db connection")
+	}
+
+	return database
+}
